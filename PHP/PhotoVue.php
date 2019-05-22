d14960d9b0a09395025960c2ec0c76b13b304c46
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-05-22 14:25:59 -0400

    Update PhotoVue.php

--- PHP/PhotoVue.php
+++ PHP/PhotoVue.php
@@ -1,52 +1,47 @@
 <!DOCTYPE html>
 
 
+
 <header>
 <link rel="stylesheet" href="CSS.PhotoVue.css">
-<header/>
-<?php $Username="Jérémie Lacroix"; 
-		$connecter=true;
-		$login_link="#login";
+
+
+<navigation>
+	<div class="topnav">
+		<a class="active" href="#home">Home</a>
+		<a href="#about">About</a>
 		
-?>
+		
+		<a style='float:right;' href='#Profil'> Username </a>
+		<a style="float:right;" href="#logout"> logout </a>
+		
+	</div>
+</navigation>
+<header/>
+
+
 
 <body>
 
-<navigation>
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  
-  <a style="float:right;" href="#logout"><?php
-	if($connecter==true){
-		echo "logout";
-	}
-  ?> </a>
-  <?php
-	if($connecter==false)
-		echo "<a style='float:right;' href='#login'> Login </a>";
-	else
-		echo "<a style='float:right;' href='#Profil'> $Username </a>";
-  ?>
-</div>
-</navigation>
 
 
-<Post>
-<div class="photolist">
-	<div class='photo'>
-		<table>
-			<tr>
-				<td><a href='#User' style='padding:10px;'>$Username</a></tb>	
-			</tr>
-			<tr>
-				<td><img src='images/allo.jpg'></tb>	
-			</tr>
-		</table>
+
+	<div class="photolist">
+		<div class='photo'>
+			<table>
+				<tr>
+					<td><a href='#User' style='padding:10px;'>$Username</a></tb>	
+				</tr>
+				<tr>
+					<td><img src='images/allo.jpg'></tb>	
+				</tr>
+			</table>
+		</div>
+
+		<input type ="text"><input type="Submit" value="Commenter">
+
+		<br>
 	</div>
 
-	<input type ="text"><input type="Submit" value="Commenter">
+</body>
 
-	<br>
-</div>
-</Post>

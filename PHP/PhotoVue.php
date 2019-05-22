72353dc19e3cb3c5473432c9a2675ccf6e1ea660
mathieu sevigny lavallee <mathieeu@outlook.com>
2019-05-22 14:09:14 -0400

    photovue

--- PHP/PhotoVue.php
+++ PHP/PhotoVue.php
@@ -34,53 +34,19 @@
 
 <Post>
 <div class="photolist">
-<p>
-<?php 
-if($connecter==false){
-	echo "you are offline please";
-	echo	"<a href='#login'>login</a>";
-	echo  "to see pictures";
-}
-else {
-	echo "
 	<div class='photo'>
 		<table>
 			<tr>
 				<td><a href='#User' style='padding:10px;'>$Username</a></tb>	
-				</tr>
-				<tr>
-				<td><img src='allo.jpg' style='width:665px; padding:10px;padding-left:10px;'></tb>	
-				</tr>
-				<tr>
-				<td><button type='button'><a href='#picture'>Comment</a></button></tb>	
+			</tr>
+			<tr>
+				<td><img src='images/allo.jpg'></tb>	
 			</tr>
 		</table>
-	</div>";
-	
-}
-?>
+	</div>
 
+	<input type ="text"><input type="Submit" value="Commenter">
 
-</p>
+	<br>
 </div>
-<div class="commentcontainer">
-	<div class="comment">	
-	User1 : allo
-	<div>
-	<div class="comment">	
-	Grand-Maman : alllo3
-	<div>
-	<div class="comment">	
-	Grand-Maman : belle photo
-	<div>
-	<div class="comment">	
-	Jé Lacroix : c'est beau
-	<div>
-	<div class="comment">	
-	charles bourgeois : lol!
-	<div>
-	
-
-<input type ="text"><input type="Submit" value="Commenter">
-
 </Post>

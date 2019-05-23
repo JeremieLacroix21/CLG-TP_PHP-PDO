9ab52112a96baf44f04740e678efecc9e9a79ae8
Charles Bourgeois <charlesbourgeois@live.ca>
2019-05-23 17:39:18 -0400

    aa

--- PHP/Photo.php
+++ PHP/Photo.php
@@ -42,7 +42,7 @@ if ($connecter)
 <navigation>
 <div class="topnav">
   <a href="Photo.php">Galerie Photo</a>
-  <a href="Ajout.php">Ajouter une photo</a>
+  <a href="Ajouter_Photo.php">Ajouter une photo</a>
 	<?php
 	if($admin == 1){
 		echo ("<a href='Admin.php'>Admin</a>");
@@ -67,79 +67,119 @@ if ($connecter)
 </div>
 </navigation>
 
-<?php
-	$resultat = $Mybd->query("SELECT id FROM Images");
-	?>
 <table>
 	<tr>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=1" >
+    	<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=2" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow' >
+			<a href="PhotoVue.php?id=3" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=4" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=5" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=6" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
-		</td>
-	</tr>
-	<tr>
-		<td>
-		<img src='images/1.jpg' alt='Nature' >
-		</td>
-		<td>
-		<img src='images/2.jpg' alt='Snow'>
-		</td>
-		<td>
-		<img src='images/2.jpg' alt='Snow' >
-		</td>
-		<td>
-		<img src='images/2.jpg' alt='Snow'>
-		</td>
-		<td>
-		<img src='images/2.jpg' alt='Snow'>
-		</td>
-		<td>
-		<img src='images/1.jpg' alt='Nature' >
-		</td>
-		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=7" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 	</tr>
 	<tr>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=8" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=9" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow' >
+			<a href="PhotoVue.php?id=10" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=11" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/2.jpg' alt='Snow'>
+			<a href="PhotoVue.php?id=12" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=13" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 		<td>
-		<img src='images/1.jpg' alt='Nature' >
+			<a href="PhotoVue.php?id=14" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<a href="PhotoVue.php?id=15" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=16" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=17" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=18" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=19" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=20" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
+		</td>
+		<td>
+			<a href="PhotoVue.php?id=21" >
+			<img src="images/1.jpg" alt='Nature'>
+			</a>
 		</td>
 	</tr>
 </table>
+
 
 <body/>

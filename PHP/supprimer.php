35e9838970e80395f9a5f11ecca2010d13e52552
JeremieLacroix21 <Jrmie@DESKTOP-JL90G99>
2019-05-27 15:41:38 -0400

    allo

--- PHP/supprimer.php
+++ PHP/supprimer.php
@@ -1,27 +1,9 @@
-<?php
-session_start(); 
-?>
 
-<!DOCTYPE html>
-<header>
-</header>
+
+
 <?php 
 $UserToDelete = $_GET['id'];
-?>
 
-<body>
-<p>
-Êtes-vous sur de vouloir supprimer : <?php echo $UserToDelete; ?>
-</p>
-<button onclick="supprimer()"> oui</button>
-<button onclick="location.href = 'Admin.php';" >non</button>
-
-</body>
-
-<script>
-function supprimer()
-{
-	<?php 
 		try {
 			$Mybd0 = new PDO('mysql:host=167.114.152.54;dbname=dbequipe24;charset=utf8','equipe24','2hv6ai74',array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
 			$stm0 = $Mybd0->prepare("CALL deletecompte(?)" , array(PDO::ATTR_CURSOR, PDO::CURSOR_FWDONLY));
@@ -31,11 +13,8 @@ function supprimer()
 			header("url=Admin.php");
 		} catch (PDOException $e)
 				{ echo('Erreur de connexion: ' . $e->getMessage());exit();}
-	
-	
-	
+	echo "$UserToDeletese a été supprimer";
 	?>
-	
 	
 }
 </script>
